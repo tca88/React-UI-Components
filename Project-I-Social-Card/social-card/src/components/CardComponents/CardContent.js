8ac707e9988f2f96721f6a690fbdba3e83ba7bd6
tca88 <trishnaamin@gmail.com>
2019-03-18 17:11:21 -0400

    Add CardContent component and style this component in Card.css.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className="card-content">
+            <h2 className="card-title">Get Started with React</h2>
+            <p className="card-text">React makes it painless to create interactive UIs. 
+    Design simple views for each state in your application.</p>
+        <p className="card-url">reactjs.org</p>
+        </div>
+    
+    );
+};
+
+export default CardContent;
