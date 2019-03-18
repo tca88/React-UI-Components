bc7655549ebe6acdb3f68c0af2fd148ba60d15a8
tca88 <trishnaamin@gmail.com>
2019-03-18 16:49:22 -0400

    Add CardContainer and CardBanner components.

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './components/HeaderComponents/HeaderContainer';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
@@ -21,7 +21,10 @@ const App = () => {
     //   </p>
     // </div>
 
-    <div className="App"><HeaderContainer /></div>
+    <div className="App">
+    <HeaderContainer />
+    <CardContainer />
+    </div>
   );
 };
 
