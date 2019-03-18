e7244269ffd3aff01238e97960e18b06ceb9040c
tca88 <trishnaamin@gmail.com>
2019-03-18 15:47:33 -0400

    Add image thumbnail component.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Header.css';
 import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
 
 const HeaderContainer = () => {
-    return <HeaderContent />
-
+    return (
+        <div className="header-container">
+            <HeaderContent />
+            <ImageThumbnail />
+        </div>
+        );
 };
 
 export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+
+const ImageThumbnail = () => {
+    return <img className="image-thumbnail" src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" alt="Lambda School logo"/>
+
+};
+
+export default ImageThumbnail;
