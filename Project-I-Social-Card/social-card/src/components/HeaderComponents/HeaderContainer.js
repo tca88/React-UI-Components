8c8ff93681bf45a8f90afc7a3a161d310a3facbe
tca88 <trishnaamin@gmail.com>
2019-03-18 15:36:10 -0400

    Create header content component.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+    return <HeaderContent />
+
+};
+
+export default HeaderContainer;
