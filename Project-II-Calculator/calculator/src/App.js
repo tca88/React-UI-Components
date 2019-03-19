0194961d3a6a520c6866743cff75e850d8b372e1
tca88 <trishnaamin@gmail.com>
2019-03-19 16:59:25 -0400

    Add CalculatorDisplay component and style within Display.css.

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import { ClearButton, ZeroButton } from './components/ButtonComponents/ActionButton';
+import OperatorButton from './components/ButtonComponents/OperatorButton';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+
+const calcButtons = {
+  operators: ['÷', '×', '−', '+', '='],
+  numbers: [7, 8, 9, 4, 5, 6, 1, 2, 3],
+  action1: 'clear',
+  action2: 0,
+};
 
 const App = () => {
   return (
     <div className="app">
-      <CalculatorDisplay />
+            <CalculatorDisplay />
+            <div className="all-buttons">
+                <div className="action-number-buttons">
+                <ClearButton action={calcButtons} />
+                <NumberButton num={calcButtons} />
+                <ZeroButton action={calcButtons} />
+                </div>
+                <OperatorButton operator={calcButtons} />
+            </div>
     </div>
   );
 };
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,29 +1,19 @@
 import React from 'react';
 import './Display.css';
-import NumberButton from '../ButtonComponents/NumberButton';
-import { ClearButton, ZeroButton } from '../ButtonComponents/ActionButton';
-import OperatorButton from '../ButtonComponents/OperatorButton';
 
-const calcButtons = {
-    operators: ['÷', '×', '−', '+', '='],
-    numbers: [7, 8, 9, 4, 5, 6, 1, 2, 3],
-    action1: 'clear',
-    action2: 0,
-};
 
-const CalculatorDisplay = () => {
+
+const CalculatorDisplay = props => {
     return (
-        <div className="claculator-display">
-            <div className="all-buttons">
-                <div className="action-number-buttons">
-                <ClearButton action={calcButtons} />
-                <NumberButton num={calcButtons} />
-                <ZeroButton action={calcButtons} />
-                </div>
-                <OperatorButton operator={calcButtons} />
-            </div>
+        <div>
+            <div className="calculator-display">{props.text}</div>
         </div>
     );
 };
+
+CalculatorDisplay.defaultProps = {
+    text: '0',
+};
+
 
 export default CalculatorDisplay;
