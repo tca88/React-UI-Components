0c4b47266292b8706ed38f2b82d9b36659b711d8
tca88 <trishnaamin@gmail.com>
2019-03-19 15:52:58 -0400

    In Calculator project, create NumberButton component and begin styling layout.

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    // console.log(props.calcProp);
+    const { num } = props;
+    const numItems = num.numbers.map((number) =>
+        <div className="number-item" key={number}>
+            {number}
+                 </div>
+                );
+    return (
+        <div>
+            <div className="number-buttons">{numItems}</div>
+        </div>
+    );
+};
+
+export default NumberButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,24 @@
 import React from 'react';
 import './Display.css';
+import NumberButton from '../ButtonComponents/NumberButton';
+
+const calcButtons = {
+    operators: ['÷', '×', '−', '+', '='],
+    numbers: [7, 8, 9, 4, 5, 6, 1, 2, 3],
+    action1: 'clear',
+    action2: 0,
+};
+
+const CalculatorDisplay = () => {
+    return (
+        <div className="claculator-display">
+            <div className="all-buttons">
+                <div className="action-number-buttons">
+                <NumberButton num={calcButtons} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CalculatorDisplay;
